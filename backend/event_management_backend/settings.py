e3eda1841652514eeeb6fa0b583dc1b096486931
Sumit <sushantm12345@gmail.com>
2024-09-24 10:22:49 +0530

    parameters

--- backend/event_management_backend/settings.py
+++ backend/event_management_backend/settings.py
@@ -179,7 +179,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_USER = get_parameter('/myevent/EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
  
--- backend/utils/aws_utils.py
+++ backend/utils/aws_utils.py
@@ -6,16 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_REGION = os.getenv('AWS_REGION')
-
-
 # Create a session using the credentials and region
 session = boto3.Session(
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=AWS_REGION
+    region_name= 'ap-south-1'
 )
 
 
